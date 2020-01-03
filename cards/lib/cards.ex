e86a5039a205fbb9f91feeb82261902992232f25
Rajwinder <rajwinder.singh@nettycoons.com>
2020-01-03 07:04:10 -0800

    Initial drop

---
+++ cards/lib/cards.ex
@@ -0,0 +1,22 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def hello do
+    "hello there"
+  end
+
+  def create_deck do
+    ["Ace","Two","three"]
+  end
+end

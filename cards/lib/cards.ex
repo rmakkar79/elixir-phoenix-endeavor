c429806eff9a515c20e5e17d76940b43e68bc711
Rajwinder <rajwinder.singh@nettycoons.com>
2020-01-04 09:54:20 -0800

    identicon

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -12,11 +12,49 @@ defmodule Cards do
       :world
 
   """
-  def hello do
-    "hello there"
-  end
+      def hello do
+        "hello there"
+      end
 
-  def create_deck do
-    ["Ace","Two","three"]
-  end
+      def create_deck do
+        values= ["Ace","Two","three","four","five"]
+        suits = ["Spades", "Clubs","Hearts","Diamonds"]
+        for suit <-suits , value <-values do
+            "#{value} of #{suit}"
+          end
+            
+      end
+
+    def shuffle(deck) do
+      Enum.shuffle(deck)
+    end
+
+    def contains?(deck, card) do
+      Enum.member?(deck,card)
+    end
+
+    def deal(deck, hand_size) do 
+      Enum.split(deck,hand_size)
+    end
+
+    def save(deck,filename) do
+      binary=:erlang.term_to_binary(deck)
+      File.write(filename, binary)
+    end
+
+    def load (filename) do
+      case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "That file does not exist"
+      end
+    end  
+
+    def create_hand(hand_size) do
+      Cards.create_deck
+      |>Cards.shuffle
+      |>Cards.deal(hand_size)
+
+    end
+
 end
+
